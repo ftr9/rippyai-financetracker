0ad43190b815024ac7e83f1f4ffa89e5649e76d9
ftr9 <ferlandodotel16@gmail.com>
2023-05-22 06:01:43 +0545

    added balance deduction logic

--- src/api/expenses/expenses.service.ts
+++ src/api/expenses/expenses.service.ts
@@ -1,12 +1,16 @@
-import { Injectable, Query } from '@nestjs/common';
+import { Injectable, NotAcceptableException } from '@nestjs/common';
 import { PrismaService } from 'src/db/prisma/prisma.service';
 import { IaddExpenseBody } from './interfaces/addExpenseBody.interface';
 import { IgetExpenseQuery } from './interfaces/getExpenseQuery.interface';
 import { IexpensesMetricQuery } from './interfaces/expensesMetricQuery.interface';
+import { FinanceService } from '../finance/finance.service';
 
 @Injectable()
 export class ExpensesService {
-  constructor(private readonly prismaService: PrismaService) {}
+  constructor(
+    private readonly prismaService: PrismaService,
+    private readonly financeService: FinanceService,
+  ) {}
 
   private _filterExpenseQuery(query: IgetExpenseQuery) {
     const queryLimit = query.limit ? query.limit : 50;
@@ -33,9 +37,39 @@ export class ExpensesService {
   }
 
   async addExpense(body: IaddExpenseBody) {
+    //1)
+    const activeMonthlyPlan = await this.financeService.getActiveMonthlyPlan(
+      body.user,
+    );
+
+    //2)
+    if (activeMonthlyPlan.remainingExpense < body.amount) {
+      throw new NotAcceptableException(
+        'your remaining expense amount is less than the amount you provided ',
+      );
+    }
+
+    await this.financeService._deductExpenseBudget(
+      activeMonthlyPlan.id,
+      body.amount,
+    );
     const createdExpense = await this.prismaService.expenditures.create({
-      data: body,
+      data: {
+        ...body,
+        expenseBudget: activeMonthlyPlan.expenseBudget,
+        remainingExpense: activeMonthlyPlan.remainingExpense - body.amount,
+      },
     });
+
+    //3)
+    const expenseLimitThreshold =
+      (createdExpense.remainingExpense / createdExpense.expenseBudget) * 100;
+    if (expenseLimitThreshold <= 20) {
+      console.log(
+        `you have spent ${100 - expenseLimitThreshold}% of your expenses`,
+      );
+    }
+
     return createdExpense;
   }
 
@@ -48,6 +82,15 @@ export class ExpensesService {
           lte: query.to,
         },
       },
+      _sum: {
+        amount: true,
+      },
+    });
+  }
+
+  async summarizeVal() {
+    return await this.prismaService.expenditures.groupBy({
+      by: ['createdAt'],
       _sum: {
         amount: true,
       },
